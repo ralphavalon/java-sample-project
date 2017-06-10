196a19cfa498d61b4dc42b726b22f7c789cffded
Ralph Avalon <amoedo.raphael@gmail.com>
2017-06-09 22:20:41 -0300

    Adding more example tests

--- src/test/java/com/conscious/it/service/UserServiceTest.java
+++ src/test/java/com/conscious/it/service/UserServiceTest.java
@@ -1,12 +1,17 @@
 package com.conscious.it.service;
 
 
+import static java.time.Duration.ofMillis;
+
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -22,6 +27,7 @@ import com.conscious.it.model.User;
 @DisplayName("Testing the logic of UserService")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Tag("all")
 public class UserServiceTest {
 	
 	@Autowired
@@ -45,10 +51,41 @@ public class UserServiceTest {
 	
 	@DisplayName("Testing exception")
 	@Test
+	@RepeatedTest(value=2, name="{displayName} {currentRepetition}/{totalRepetitions}") //it will repeat twice, running 3 times at total
 	public void testException() {
 		Throwable exception = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> { userService.getAll().get(15); });
 		Assertions.assertEquals("Index: 15, Size: 2", exception.getMessage());
 	}
+	
+	@Test
+	@Tag("example")
+    void groupedAssertions() {
+        // In a grouped assertion all assertions are executed, and any
+        // failures will be reported together.
+		final User user = userService.getById(1);
+        Assertions.assertAll("user",
+            () -> Assertions.assertEquals("First User", user.getName()),
+            () -> Assertions.assertEquals(new Integer(1), user.getId()),
+            () -> Assertions.assertEquals(new Integer(20), user.getAge()),
+            () -> Assertions.assertEquals("Rio de Janeiro", user.getCity())
+        );
+    }
+	
+	@Test
+    @Disabled("For demonstration purposes")
+    public void skippedTest() {
+        // not executed
+		Assertions.assertTrue(false);
+    }
+	
+	@Test
+	@Tag("example")
+    void timeoutExceeded() {
+        Assertions.assertTimeout(ofMillis(100), () -> {
+            // Simulate task that takes less than 100 ms.
+            Thread.sleep(99); // If more than 100, it will fail due timeout assert
+        });
+    }
 	
 	protected static Stream<Arguments> createUsers() {
 	    return Stream.of(
