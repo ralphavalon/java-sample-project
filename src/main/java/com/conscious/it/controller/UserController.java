8d7175dc7ea111fac7d62b9594e5890fa74a3ceb
Ralph Avalon <amoedo.raphael@gmail.com>
2018-01-22 23:00:59 -0200

    Changing for system tests

--- src/main/java/com/conscious/it/controller/UserController.java
+++ src/main/java/com/conscious/it/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.conscious.it.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,12 +20,16 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String getAll() {
+	public String getAll(User user) {
 		return "home";
 	}
 	
 	@RequestMapping(value="/create", method=RequestMethod.POST)
-	public String create(Model model, User user) {
+	public String create(Model model, @Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "home";
+		}
+		
 		userService.save(user);
 		model.addAttribute("user", user);
 		return "success";
--- src/main/java/com/conscious/it/model/User.java
+++ src/main/java/com/conscious/it/model/User.java
@@ -1,9 +1,17 @@
 package com.conscious.it.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class User {
@@ -11,15 +19,23 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	@NotBlank
 	private String name;
+	@NotBlank
 	private String city;
-	private Integer age;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	@Size(min=6, max=20)
+	private String password;
+	@NotNull
+	private Gender gender;
+	@NotNull
+	@DateTimeFormat(pattern="dd/MM/yyyy") //Error message is in messages.properties
+	private LocalDate birthdate;
 	
 	public User() {}
-	public User(String name, Integer age) {
-		this.name = name;
-		this.age = age;
-	}
 	
 	public Integer getId() {
 		return id;
@@ -39,12 +55,33 @@ public class User {
 	public void setCity(String city) {
 		this.city = city;
 	}
-	public Integer getAge() {
-		return age;
+	public String getEmail() {
+		return email;
 	}
-	public void setAge(Integer age) {
-		this.age = age;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	enum Gender {
+		MALE, FEMALE
+	}
 
 }
